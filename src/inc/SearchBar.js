1e41f58700ec766251eb7b8cb27fca368b1404f9
Pietari Tanner <cotapi01@students.oamk.fi>
2021-12-08 17:31:31 +0200

    search tehty uusiksi

--- src/inc/SearchBar.js
+++ src/inc/SearchBar.js
@@ -1,22 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export default function SearchBar({addToCart}) {
+export default function SearchBar({ url, addToCart }) {
 
     const [allData, setAllData] = useState([]);
-    const [filteredData, setFilteredData] = useState([]);
-    const [input, setInput] = useState([]);
+    const [searchItem, setSearchItem] = useState(null);
+    const [searchTerm, setSearchTerm] = useState('');
 
-    // Hakee kaikki tuotteet product taulusta
+    // Hakee kaikki tuotteet product ja tarjous taulusta
     useEffect(() => {
         if (allData !== null) {
-            const address = "http://localhost/verkkokauppa/products/searchproducts.php/"
-
+            const address = url + "products/searchproducts.php/";
             axios.get(address)
                 .then((response) => {
                     const json = response.data;
                     setAllData(json);
-                    //console.log(allData)
+                    console.log(allData)
                 }).catch(error => {
                     if (error.response === undefined) {
                         alert(error);
@@ -25,39 +24,32 @@ export default function SearchBar({addToCart}) {
                     }
                 })
         }
-
     }, [])
 
-    //Vertaa input arvoa product taulun arvoon
-    function handleSearch() {
-
-        let result = [];
-        result = allData.filter((data) => {
-            if (input === data.name) {
-                setFilteredData(data);
-            }
-        });
-
-    }
-    
     return (
-        <div>
-            <h3>Etsi tuotteita</h3>
-            <input type="text" placeholder="Etsi tuote nimellä" onChange={event => setInput(event.target.value)}></input>
-            <button onClick={(event) => handleSearch(event)}>Etsi</button>
-         <div>
-            
-                <div>
-                    <p>{filteredData.name}</p>
-                    <p>{filteredData.price}</p>
-                     <button className="btn btn-primary" type="button" onClick={e => addToCart(filteredData)}>Lisää ostoskoriin</button>
-                    
-                    <div>
-                        {/* <img src={url + 'images/' + product.image} alt="" /> */}
-                    </div>
-                </div>
-                
-           </div> 
-         </div>       
-    );
+        <div className="container">
+            <h1>Kaikki Tuotteet</h1>
+            <input type="text" placeholder="Etsi tuotteita"
+                onChange={e => { setSearchTerm(e.target.value); }} />
+
+            {allData.filter((item) => {
+                if (searchTerm == '') {
+                    return item
+                } else if (item.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                    return item
+                }
+            }).map((item, id) => {
+                return (           
+                    <div key={id} onClick={e => setSearchItem(item)}>
+                        <div>
+                            <img src={url + 'images/' + item.image} alt={item.name} className="pikkukuva" />
+                        </div>
+                        <p>{item.name}</p>
+                        <p>{item.price}</p>
+                        <button className="btn btn-primary" type="button" onClick={e => addToCart(item)}>Lisää ostoskoriin</button>
+                    </div>               
+                );
+            })}
+        </div>
+    )
 }
